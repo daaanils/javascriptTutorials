c544838fcf7fd3cb823dea542557601db3982367
Danilo Jr. Pelaso <72912298+daaanils@users.noreply.github.com>
2023-04-19 14:36:07 +0800

    Create Loop.js

---
+++ Loop.js
@@ -0,0 +1,13 @@
+// ***FOR LOOPS
+
+const fruits = ['banana', 'apple', 'orange', 'grapes', 'mango','banana', 'apple', 'orange', 'grapes', 'mango','banana', 'apple', 'orange', 'grapes', 'mango','banana', 'apple', 'orange', 'grapes', 'mango']
+
+//Old for loops
+for (let i=0; i < fruits.length; i++) {
+    // console.log(fruits[i])
+}
+
+//Modern For loops 
+for (const prutas of fruits) {
+    // console.log(prutas)
+}
