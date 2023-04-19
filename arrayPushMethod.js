844ad709ae6d385643039175e0411fa87684657f
Danilo Jr. Pelaso <72912298+daaanils@users.noreply.github.com>
2023-04-19 14:28:56 +0800

    Create arrayPushMethod.js

---
+++ arrayPushMethod.js
@@ -0,0 +1,10 @@
+const groceries = ['banana', 'apple', 'orange', 'mango']
+// console.log(groceries[2])
+
+
+//Array methods
+groceries.push('cookies')
+groceries.push('cookies')
+ groceries.push('blueberries')
+
+console.log(groceries)
