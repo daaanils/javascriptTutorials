5d22e2bac3f391ab267d3fde144d8601bfbf2250
Danilo Jr. Pelaso <72912298+daaanils@users.noreply.github.com>
2023-04-19 14:19:34 +0800

    Create sumFunctions.js

---
+++ sumFunctions.js
@@ -0,0 +1,8 @@
+//This is the function in Javascript ES6 and with implicit return
+const sumArrow = (a, b) => a + b
+
+
+//This is the function with explicit return
+const summ = (a, b) => {
+    return a + b
+}
