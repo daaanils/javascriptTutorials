f44942621eecd27954004c6a7e8d831b37a03ff4
Danilo Jr. Pelaso <72912298+daaanils@users.noreply.github.com>
2023-09-28 21:29:03 +0800

    Create Destructuring.js

---
+++ Destructuring.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Danilo Pelaso',
+    age: 21,
+    birthdate: new Date(),
+    status: 'Single'
+};
+
+const {name, age, birthdate, status} = person;
+
+
+
+console.log(name, age, birthdate, status);
